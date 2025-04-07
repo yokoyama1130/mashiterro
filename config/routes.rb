5ca696f951bf098fa8a3342455ae912f2b2df211
EC2 Default User <ryogotan@ezweb.ne.jp>
2025-04-07 09:41:55 +0000

    meshiterro2-7

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # # 
+  # root to: "homes#about"
+  get '/homes/about', to: 'homes#about', as: "about"
+  # #
 end
 
 
