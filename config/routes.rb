c8bc008aae76853144b6e6f53fb39ff764d70ab0
EC2 Default User <ryogotan@ezweb.ne.jp>
2025-04-09 20:48:44 +0000

    meshiterro2-9finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  # get 'post_images/new'
+  # get 'post_images/index'
+  # get 'post_images/show'
+  resources :post_images, only: [:new, :index, :show]
+  
   devise_for :users
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # # 
   # root to: "homes#about"
-  get '/homes/about', to: 'homes#about', as: "about"
+  get '/homes/about' => 'homes#about', as: "about"
   # #
 end
 
