e38c695faac3c0a22d167b6a578910a7a17c4ac4
EC2 Default User <ryogotan@ezweb.ne.jp>
2025-04-16 07:00:11 +0000

    meshiterro22finished

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -5,6 +5,9 @@ class PostImage < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  validates :shop_name, presence: true
+  validates :image, presence: true
+
 
 
   def get_image
@@ -18,5 +21,5 @@ class PostImage < ApplicationRecord
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
 end
