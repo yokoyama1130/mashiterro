03ce3f3c9613a20db1402797296c4f14f02a958b
EC2 Default User <ryogotan@ezweb.ne.jp>
2025-04-16 07:47:18 +0000

    meshiterro25finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     # URLに記載されたIDを参考に、必要なモデルを取得する
     @user = User.find(params[:id])
@@ -25,6 +27,13 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
   end
 
 end
